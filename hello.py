ce60bb0c5175a7f70749cb73990bbb0f8b19600d
Max Weaver <max.weaver@infectiousmedia.com>
2018-01-05 09:01:35 +0000

    2c

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask_script import Manager
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
@@ -13,4 +15,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
