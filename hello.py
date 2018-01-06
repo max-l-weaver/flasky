d6cbd5e9710807a0be744509302fddea5edc8fa9
Max Weaver <max.weaver@infectiousmedia.com>
2018-01-06 20:32:55 +0000

    adding form functionality

--- hello.py
+++ hello.py
@@ -4,11 +4,21 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'H@rd2GU355%t!ng!'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -19,10 +29,16 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    name = None
+    form = NameForm()
+    # print(dir(form)
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
     return render_template('index.html',
-                           current_time=datetime.utcnow())
+                           form=form, name=name)
 
 
 @app.route('/user/<name>')
