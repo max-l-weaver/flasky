aa599a0d06421fa1cfbaa2329409a1f529e49952
Max Weaver <max.weaver@infectiousmedia.com>
2018-01-06 23:10:18 +0000

    added first models

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
+import os
 from datetime import datetime
 
 from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -8,11 +10,37 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
 app.config['SECRET_KEY'] = 'H@rd2GU355%t!ng!'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(Form):
